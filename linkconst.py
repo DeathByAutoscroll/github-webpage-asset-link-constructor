e7c5a602a0b607f5077c2d08e0b4e8248c901c49
DeathByAutoscroll <39137649+DeathByAutoscroll@users.noreply.github.com>
2023-12-09 23:54:50 +0000

    Convert backslashes to regular slashes

--- linkconst.py
+++ linkconst.py
@@ -4,14 +4,17 @@ import glob
 
 # Point mypath to the highest folder you want all the assets for e.g. "C:\Users\Baliff\Documents\GitHub\Meekins.github.io".
 # Your username is what appears in the "Username.github.io" link
-mypath=r"INSERT_FILEPATH_HERE"
-githubUsername="ExampleUser"
+mypath=r"INSERT FILEPATH HERE"
+githubUsername="EXAMPLE"
 
 # Change r'\**\*.gif' to whatever extension you want to generate links for.
-files = glob.glob(mypath + r'\**\*.gif', recursive=True)
+files = glob.glob(mypath + r'\**\*.*', recursive=True)
 
 # print(files) # as list
 for f in files:
     splitResult = f.split(".github.io")
     splitResult = "https://" + githubUsername + ".github.io" + splitResult[1]
-    print(splitResult) # nice looking single line per file
+
+    replaceResult = splitResult.replace("\\", "/" ) # Double backslash is needed to prevent escape issues.
+
+    print(replaceResult) # nice looking single line per file
